7c045019f8d6320addbad1476ab2b07c1b989720
Tu Mai Doan <tumai.doan@haw-hamburg.de>
2021-04-16 20:15:16 +0200

    init version

---
+++ src/main/java/com/haw/bikeservice/eBikeMe/EBikeMeApplication.java
@@ -0,0 +1,13 @@
+package com.haw.bikeservice.eBikeMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBikeMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBikeMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haw/bikeservice/eBikeMe/EBikeMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haw.bikeservice.eBikeMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBikeMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
